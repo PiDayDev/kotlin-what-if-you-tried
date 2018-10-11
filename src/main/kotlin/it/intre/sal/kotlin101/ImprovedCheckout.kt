f7b03aca0c9818397d795312273bedad73c3c33f
Salvi <salvi@intre.it>
2018-10-11 14:41:14 +0200

    Get rid of test exceptions

--- src/main/kotlin/it/intre/sal/kotlin101/ImprovedCheckout.kt
+++ src/main/kotlin/it/intre/sal/kotlin101/ImprovedCheckout.kt
@@ -2,8 +2,6 @@ package it.intre.sal.kotlin101
 
 class ImprovedCheckout : Checkout {
 
-    override fun pay(items: List<String>, offers: Map<String, Map.Entry<Int, Int>>): Int {
-        TODO("not implemented")
-    }
+    override fun pay(items: List<String>, offers: Map<String, Map.Entry<Int, Int>>) = 0
 
 }
