60616d44863530d95042974a2257f74f4a576141
Salvi <salvi@intre.it>
2018-10-11 15:04:23 +0200

    Implementing: some constants

--- src/main/kotlin/it/intre/sal/kotlin101/ImprovedCheckout.kt
+++ src/main/kotlin/it/intre/sal/kotlin101/ImprovedCheckout.kt
@@ -1,6 +1,18 @@
 package it.intre.sal.kotlin101
 
+const val APPLE = "apple"
+const val PEAR = "pear"
+const val PINEAPPLE = "pineapple"
+const val BANANA = "banana"
+
 class ImprovedCheckout : Checkout {
+
+    private val map = mapOf(
+        APPLE to 50,
+        PEAR to 30,
+        PINEAPPLE to 220,
+        BANANA to 60
+    )
 
     override fun pay(items: List<String>, offers: Map<String, Map.Entry<Int, Int>>) = 0
 
