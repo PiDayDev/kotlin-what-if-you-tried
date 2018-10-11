4e06450af9895c643de1194c78ad42ffb1bc5f82
Salvi <salvi@intre.it>
2018-10-11 15:56:37 +0200

    Refactor: naming, casts

--- src/main/kotlin/it/intre/sal/kotlin101/ImprovedCheckout.kt
+++ src/main/kotlin/it/intre/sal/kotlin101/ImprovedCheckout.kt
@@ -9,76 +9,68 @@ const val BANANA = "banana"
 
 class ImprovedCheckout : Checkout {
 
-    private val map = mapOf(
-        APPLE to 50,
-        PEAR to 30,
-        PINEAPPLE to 220,
-        BANANA to 60
+    private val prices = mapOf(
+            APPLE to 50,
+            PEAR to 30,
+            PINEAPPLE to 220,
+            BANANA to 60
     )
 
     override fun pay(items: List<String>, offers: Map<String, Entry<Int, Int>>): Int {
         var res = 0
-        var a = 0
-        var p = 0
-        var ananas = 0
-        var b = 0
+        var apple = 0
+        var pear = 0
+        var pineapple = 0
+        var banana = 0
 
         for (item in items) {
             when (item) {
-                APPLE -> a++
-                PEAR -> p++
-                PINEAPPLE -> ananas++
-                BANANA -> b++
+                APPLE -> apple++
+                PEAR -> pear++
+                PINEAPPLE -> pineapple++
+                BANANA -> banana++
             }
         }
 
-
-        //Here I have to cycle through every offer to see if it applies
-        for ((key, value) in offers) {
-            when (key) {
-                "apple" -> {
-                    val a1 = (value as Entry<*, *>).key as Int
-                    if (a >= a1) {
-                        res += (value as Entry<*, *>).value as Int
+        for ((item, offer) in offers) {
+            when (item) {
+                APPLE -> {
+                    val a1 = offer.key
+                    if (apple >= a1) {
+                        res += offer.value
                     }
-                    a -= a1
+                    apple -= a1
                 }
-                //jb 2008-09-12: don't sell lychee anymore, but maybe in the future...
-                //                case "lychee":
-                //                    int a2 = (int) ((Entry) entry.getValue()).getKey();
-                //                    if (p >= a2) { res += (int) ((Entry) entry.getValue()).getValue(); }
-                //                    p -= a2;
-                //                    break;
-                "pear" -> {
-                    val a2 = (value as Entry<*, *>).key as Int
-                    if (p >= a2) {
-                        res += (value as Entry<*, *>).value as Int
+                PEAR -> {
+                    val a2 = offer.key
+                    if (pear >= a2) {
+                        res += offer.value
                     }
-                    p -= a2
+                    pear -= a2
                 }
-                "pineapple" -> {
-                    val a3 = (value as Entry<*, *>).key as Int
-                    if (ananas >= a3) {
-                        res += (value as Entry<*, *>).value as Int
+                PINEAPPLE -> {
+                    val a3 = offer.key
+                    if (pineapple >= a3) {
+                        res += offer.value
                     }
-                    ananas -= a3
+                    pineapple -= a3
                 }
-                "banana" -> {
-                    val a4 = (value as Entry<*, *>).key as Int
-                    if (b >= a4) {
-                        res += (value as Entry<*, *>).value as Int
+                BANANA -> {
+                    val a4 = offer.key
+                    if (banana >= a4) {
+                        res += offer.value
                     }
-                    b -= a4
+                    banana -= a4
                 }
             }
         }
 
-        for ((key, value) in map) {
-            when (key) {
-                APPLE -> res += a * value
-                PEAR -> res += p * value
-                PINEAPPLE -> res += ananas * value
-                BANANA -> res += b * value
+        for ((item, price) in prices) {
+            when (item) {
+                APPLE -> res += apple * price
+                PEAR -> res += pear * price
+                PINEAPPLE -> res += pineapple * price
+                BANANA -> res += banana * price
             }
         }
 
